a28c742d2b7373eaf2f85ec214d0370017422d22
emiliotamo <emtamayom01@educarex.es>
2025-06-06 12:06:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ if (!process.env.OPENAI_API_KEY) {
 class ApiService {
   constructor(apiKey) {
     this.axiosInstance = axios.create({
-      baseURL: 'http://localhost:5010/api/OpenDataSig',
+      baseURL: 'https://geocaceres.onrender.com/api/OpenDataSig',
       headers: {
         'Content-Type': 'application/json',
         'OpenAi-ApiKey': apiKey,
